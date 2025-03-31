67ecb3c8e707b3ce32a04e3f37d309d7ef544792
nikhil <nikhilchawla9013@gmail.com>
2025-03-31 16:53:42 +0530

    add mongo db

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const {engine} = require('express-handlebars');
 
 const app = express();
 const errorController = require("./controllers/error");
-const {router:adminRoutes} = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const sequelize = require("./util/database");
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cartItem');
+// const {router:adminRoutes} = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cartItem');
+const {connectToDb} = require('./util/database');
 
 //handlebars
 //for express handlebar we need to tell express it exists for pug we dont as it is kind of built in
@@ -33,63 +33,18 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use((req,res,next)=>{
-    User.findByPk(1).then((_)=>{
-        req.user = _;
-        next();
-    }).catch(_=>console.log(_.message));
-})
-
 app.get("/",(req,res)=>{
     res.send("Healthy server");
 })
 
-app.use('/admin', adminRoutes);
-app.use('/shop',shopRoutes);
-
+// app.use('/admin', adminRoutes);
+// app.use('/shop',shopRoutes);
 
 app.use(errorController.get404);
 
-
-
-// we can define other one also
-Product.belongsTo(User, {
-  constraints: true,
-  onDelete: "CASCADE",
-});
-
-User.hasMany(Product);
-
-User.hasOne(Cart);
-// Cart.belongsTo(User);
-
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart,{through: CartItem});
-
-sequelize
-// .sync({force: true})
-.sync()
-.then((_)=>{
-    return User.findByPk(1);
-    // console.log(_);
-})
-.then((user)=>{
-    console.log("User", user);
-    if(!user) {
-        return User.create({
-            name:"Nikhil",
-            emailId:"nikhilchawla9013@gmail.com"
-        });
-    }
-    return Promise.resolve(user);
-}).then(user=>{
-    return user.createCart();
-}).then(_=>{
+connectToDb(()=>{
     app.listen(3000,()=>{
         console.log("listening on port 3000");
     });
 })
-.catch((_)=>{
-    console.log("Error",_);
-});
 
--- util/database.js
+++ util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("ecommerce_website",'root','chawlA1234!',{
-    dialect:'mysql',
-    host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const connectToDb = (callback) => {
+    MongoClient.connect("mongodb+srv://nikhilchawla9013:1icVCiLKJGHdvfSw@cluster0.of1mhqj.mongodb.net/ecommerce-app").then((_)=>{
+        console.log("connected");
+        callback();
+        _db = _.db();
+    }).catch((e)=>{
+        console.log("not connected",e?.message);
+        throw e;
+    })
+}
+
+const getDb = () => {
+    if(_db) return _db;
+    throw Error("No Db Found");
+}
+
+module.exports = {
+    connectToDb,
+    getDb
+};
